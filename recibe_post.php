fb4a7c6a5ae19fa46a51b7b8df4c4aefe2805371
Yani231198 <yanies.gomez@gmail.com>
2022-02-11 14:38:14 -0600

    Inicio de Proyecto

---
+++ recibe_post.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos post</title>
+</head>
+<body>
+ 
+Nombre: <?php echo $_POST['Nombre']?> <br>
+Usuario: <?php echo $_POST['Usuario']?> <br>
+Password: <?php echo $_POST['Password']?> <br>
+Password2: <?php echo $_POST['Password2']?> <br>
+Email: <?php echo $_POST['Email']?> <br>
+</body>
+</html>
